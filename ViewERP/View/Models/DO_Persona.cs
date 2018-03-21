d97613cacb63e514750bfd38891ff12c980580a6
rbanuelosdiaz <raul.banuelos.diaz@hotmail.com>
2018-03-21 15:58:57 -0600

    Se agrega un nuevo proyecto

---
+++ ViewERP/View/Models/DO_Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace View.Models
+{
+    public class DO_Persona
+    {
+        public int idUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public int idRol { get; set; }
+        public int idCompania { get; set; }
+
+        public string NombreCompleto
+        {
+            get
+            {
+                return Nombre + " " + ApellidoPaterno + " " + ApellidoMaterno;
+            }
+        }
+    }
+}
