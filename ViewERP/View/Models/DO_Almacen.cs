532138cb9fc9859aafebbcf3452c5e19c34986e8
rbanuelosdiaz <raul.banuelos.diaz@hotmail.com>
2018-03-23 08:09:29 -0600

    .

---
+++ ViewERP/View/Models/DO_Almacen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace View.Models
+{
+    [Table("TBL_ALMACEN")]
+    public class DO_Almacen
+    {
+        [Key]
+        public int idAlmacen { get; set; }
+        public int idCompania { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
