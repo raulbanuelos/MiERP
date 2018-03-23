9ea3e910b123659cbf1b628056d86dd618f3f958
PixieTaxi <taxi.pixie@gmail.com>
2018-03-22 22:03:56 -0600

    Avance

---
+++ ViewERP/View/Controllers/AlmacenController.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View.Models;
+
+namespace View.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        // GET: Almacen
+        public ActionResult Index()
+        {
+            return View(DataManager.GetAllAlmacen());
+        }
+
+        public ActionResult Create(DO_Almacen almacen = null)
+        {
+            if (!string.IsNullOrEmpty(almacen.Nombre))
+            {
+                DataManager.InsertAlmacen(almacen);
+                return RedirectToAction("Index", "Almacen");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
