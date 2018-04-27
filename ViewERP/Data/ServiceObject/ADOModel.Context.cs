68c13c1e8ac8e019386fe6712346a4eee588246e
PixieTaxi <taxi.pixie@gmail.com>
2018-04-26 20:06:25 -0500

    local

--- ViewERP/Data/ServiceObject/ADOModel.Context.cs
+++ ViewERP/Data/ServiceObject/ADOModel.Context.cs
@@ -33,10 +33,10 @@ namespace Data.ServiceObject
         public virtual DbSet<TBL_UNIDAD> TBL_UNIDAD { get; set; }
         public virtual DbSet<TBL_USUARIO> TBL_USUARIO { get; set; }
         public virtual DbSet<TBL_ARTICULO> TBL_ARTICULO { get; set; }
-        public virtual DbSet<TBL_MOVIMIENTO_ALMACEN> TBL_MOVIMIENTO_ALMACEN { get; set; }
         public virtual DbSet<TBL_EXISTENCIA> TBL_EXISTENCIA { get; set; }
         public virtual DbSet<TBL_DETALLE_MOVIMIENTO_SALIDA_ALMACEN> TBL_DETALLE_MOVIMIENTO_SALIDA_ALMACEN { get; set; }
         public virtual DbSet<TBL_MOVIMIENTO_SALIDA_ALMACEN> TBL_MOVIMIENTO_SALIDA_ALMACEN { get; set; }
         public virtual DbSet<TBL_ALERTAS_STOCK_MIN> TBL_ALERTAS_STOCK_MIN { get; set; }
+        public virtual DbSet<TBL_MOVIMIENTO_ALMACEN> TBL_MOVIMIENTO_ALMACEN { get; set; }
     }
 }
