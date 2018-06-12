a25656f377aab314c03869426f4b8c06ecca4103
M0051722 <M0051722@MXAG-BANUELOS-D>
2018-06-12 16:53:18 -0500

    reporte entradas almacen.

--- ViewERP/View/Controllers/ReportesController.cs
+++ ViewERP/View/Controllers/ReportesController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using View.Models;
 
 namespace View.Controllers
 {
@@ -12,7 +14,30 @@ namespace View.Controllers
         [ERPVerificaRol]
         public ActionResult EntradasArticulos()
         {
+            int idCompania = ((DO_Persona)Session["UsuarioConectado"]).idCompania;
+
+            List<SelectListItem> proveedores = DataManager.ConvertListDOProveedorToSelectListItem(DataManager.GetAllProveedor(idCompania));
+            proveedores.Add(new SelectListItem { Text = "Ninguno", Value = "0", Selected = true });
+            ViewBag.Proveedores = proveedores;
+
+            List<SelectListItem> almacenes = DataManager.ConvertListDOAlmacenToSelectListItem(DataManager.GetAllAlmacen(idCompania));
+            almacenes.Add(new SelectListItem { Text = "Ninguno", Value = "0", Selected = true });
+            ViewBag.Almacenes = almacenes;
+
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetEntradasArticulos(string fechaInicial, string fechaFinal,string noFactura,string usuario,int idAlmacen,int idProveedor,int idArticulo)
+        {
+            List<DO_ReporteEntradaArticulo> lista = new List<DO_ReporteEntradaArticulo>();
+
+            lista = DataManager.GetReporteEntrada(fechaInicial, fechaFinal, noFactura, usuario, idAlmacen, idProveedor, idArticulo);
+
+            var jsonResult = Json(lista, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
         }
     }
 }
