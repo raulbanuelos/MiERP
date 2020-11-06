e4fa63c4b17138df2d22a5ea25feaf47eb4f4dde
Raul <raul.banuelos.diaz@hotmail.com>
2020-11-06 16:00:36 -0600

    Validacion de correo al crear una cuenta.

--- ViewERP/WebView/Controllers/CreateAccountController.cs
+++ ViewERP/WebView/Controllers/CreateAccountController.cs
@@ -20,62 +20,63 @@ namespace WebView.Controllers
         [HttpPost]
         public JsonResult CrearCuenta(string nombre, string email, string contrasena )
         {
-            int idCompania = DataManager.InsertCompania(nombre, "RFC", "Dirección", "Telefono", email);
 
-            if (idCompania > 0)
+            DO_Persona dO_Persona1 =  DataManager.GetPersona(email);
+
+            if (dO_Persona1.ID_ROL == 2 || dO_Persona1.idUsuario == 0)
             {
-                DO_Persona dO_Persona = new DO_Persona();
-                dO_Persona.Nombre = nombre;
-                dO_Persona.ApellidoMaterno = string.Empty;
-                dO_Persona.ApellidoPaterno = string.Empty;
-                dO_Persona.idCompania = idCompania;
-                dO_Persona.ID_ROL = 1;
-                dO_Persona.Contrasena = contrasena;
-                dO_Persona.Usuario = email;
+                int idCompania = DataManager.InsertCompania(nombre, "RFC", "Dirección", "Telefono", email);
 
-                int r = DataManager.InsertPersona(dO_Persona);
+                if (idCompania > 0)
+                {
+                    DO_Persona dO_Persona = new DO_Persona();
+                    dO_Persona.Nombre = nombre;
+                    dO_Persona.ApellidoMaterno = string.Empty;
+                    dO_Persona.ApellidoPaterno = string.Empty;
+                    dO_Persona.idCompania = idCompania;
+                    dO_Persona.ID_ROL = 1;
+                    dO_Persona.Contrasena = contrasena;
+                    dO_Persona.Usuario = email;
 
-                DO_Almacen almacen = new DO_Almacen();
-                almacen.idCompania = idCompania;
-                almacen.Nombre = "Mi Bodega";
-                almacen.Descripcion = "Esta es mi bodega";
+                    int r = DataManager.InsertPersona(dO_Persona);
 
-                int rAlmacen = DataManager.InsertAlmacen(almacen);
+                    DO_Almacen almacen = new DO_Almacen();
+                    almacen.idCompania = idCompania;
+                    almacen.Nombre = "Mi Bodega";
+                    almacen.Descripcion = "Esta es mi bodega";
 
-                DO_CategoriaArticulo categoriaArticulo = new DO_CategoriaArticulo();
+                    int rAlmacen = DataManager.InsertAlmacen(almacen);
 
-                categoriaArticulo.idCompania = idCompania;
-                categoriaArticulo.NombreCategoria = "ARTICULOS DE VENTA";
-                categoriaArticulo.numeroCategoria = "01";
+                    DO_CategoriaArticulo categoriaArticulo = new DO_CategoriaArticulo();
 
-                int c = DataManager.InsertCategoriaArticulo(categoriaArticulo);
+                    categoriaArticulo.idCompania = idCompania;
+                    categoriaArticulo.NombreCategoria = "ARTICULOS DE VENTA";
+                    categoriaArticulo.numeroCategoria = "01";
 
-                DO_CategoriaArticulo categoriaArticulo1 = new DO_CategoriaArticulo();
-                categoriaArticulo1.idCompania = idCompania;
-                categoriaArticulo1.NombreCategoria = "OTROS";
-                categoriaArticulo1.numeroCategoria = "02";
+                    int c = DataManager.InsertCategoriaArticulo(categoriaArticulo);
 
-                c = DataManager.InsertCategoriaArticulo(categoriaArticulo1);
+                    DO_CategoriaArticulo categoriaArticulo1 = new DO_CategoriaArticulo();
+                    categoriaArticulo1.idCompania = idCompania;
+                    categoriaArticulo1.NombreCategoria = "OTROS";
+                    categoriaArticulo1.numeroCategoria = "02";
 
-                //DO_Proveedor proveedor = new DO_Proveedor();
-                //proveedor.Correo = "miproveedor@email.com";
-                //proveedor.Direccion = "Dirección";
-                //proveedor.idCompania = idCompania;
-                //proveedor.Nombre = "BODEGA ORIGEN 1";
-                //proveedor.RFC = "RFC";
-                //proveedor.Telefono1 = "TELEFONO 1";
-                //proveedor.Telefono2 = "TELEFONO 2";
-                //int rp = DataManager.InsertProveedor(proveedor);
+                    c = DataManager.InsertCategoriaArticulo(categoriaArticulo1);
 
-                var jsonResult = Json(r, JsonRequestBehavior.AllowGet);
+                    var jsonResult = Json("Cuenta activada exitosamente", JsonRequestBehavior.AllowGet);
+                    jsonResult.MaxJsonLength = int.MaxValue;
+
+                    return jsonResult;
+                }
+            }
+            else
+            {
+                var jsonResult = Json("El correo ya existe, por favor ingresa otro.", JsonRequestBehavior.AllowGet);
                 jsonResult.MaxJsonLength = int.MaxValue;
 
                 return jsonResult;
-
             }
 
             return null;
-            
         }
     }
 }
