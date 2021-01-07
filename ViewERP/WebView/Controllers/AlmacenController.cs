a54aeaae11ed9989fe69560ff2be599e76acf8db
Raul <raul.banuelos.diaz@hotmail.com>
2021-01-07 16:32:50 -0600

    Integración de varios almacenes.

---
+++ ViewERP/WebView/Controllers/AlmacenController.cs
@@ -0,0 +1,65 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebView.Models;
+
+namespace WebView.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        // GET: Almacen
+        public ActionResult Index()
+        {
+            return View(DataManager.GetAllAlmacen(((DO_Persona)Session["UsuarioConectado"]).idCompania));
+        }
+
+        [HttpPost]
+        public JsonResult Get(string parametro)
+        {
+            var jsonResult = Json(DataManager.GetAllAlmacen(((DO_Persona)Session["UsuarioConectado"]).idCompania), JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
+        }
+
+        [ERPVerificaRol]
+        public ActionResult Create(DO_Almacen almacen = null)
+        {
+            if (!string.IsNullOrEmpty(almacen.Nombre))
+            {
+                almacen.idCompania = ((DO_Persona)Session["UsuarioConectado"]).idCompania;
+                DataManager.InsertAlmacen(almacen);
+                return RedirectToAction("Index", "Almacen");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [ERPVerificaRol]
+        public ActionResult Edit(int id = 0, DO_Almacen almacen = null)
+        {
+            if (id != 0 && almacen.idAlmacen == 0)
+            {
+                return View(DataManager.GetAlmacen(id));
+            }
+            else
+            {
+                almacen.idCompania = ((DO_Persona)Session["UsuarioConectado"]).idCompania;
+                DataManager.UpdateAlamcen(almacen);
+                return RedirectToAction("Index", "Almacen");
+            }
+        }
+
+        [ERPVerificaRol]
+        public ActionResult Delete(int id = 0)
+        {
+            DataManager.DeleteAlmacen(id);
+            return RedirectToAction("Index", "Almacen");
+        }
+    }
+}
--- ViewERP/WebView/Controllers/ExistenciasController.cs
+++ ViewERP/WebView/Controllers/ExistenciasController.cs
@@ -28,6 +28,19 @@ namespace WebView.Controllers
         }
 
         [HttpPost]
+        public JsonResult GetExistenciasByAlmacen(int idAlmacen)
+        {
+            List<DO_Existencia> dO_Existencias = DataManager.GetExistenciaArticulos(idAlmacen);
+
+            dO_Existencias = dO_Existencias.OrderByDescending(x => x.Cantidad).ToList();
+
+            var jsonResult = Json(dO_Existencias, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
+        }
+
+        [HttpPost]
         public JsonResult GetExistenciasByCompania(int idCompania)
         {
             //Codigo para obtener el id del almacen de la persona conectada.
